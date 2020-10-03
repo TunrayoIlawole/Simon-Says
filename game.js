2c1d5965a7f3f07f3bd82b64402af606665fd3ef
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2020-10-03 16:14:50 +0100

    create colour arrays

---
+++ game.js
@@ -0,0 +1,8 @@
+// store the buttons in an array
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+// this stores the pattern in which random button colours are chosen
+const gamePattern = [];
+
+// this stores the pattern in which the user clicks on the buttons
+let userClickedPattern = [];

ed056bf7f6d021e8f1b4c1b2add96337d81babbf
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2020-10-03 16:16:31 +0100

    Set up keypress

--- game.js
+++ game.js
@@ -6,3 +6,19 @@ const gamePattern = [];
 
 // this stores the pattern in which the user clicks on the buttons
 let userClickedPattern = [];
+
+// at the start of the game:
+let level = 0;
+
+let started = false;
+
+
+// to start the game, a key on the keyboard is pressed.
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text(`Level ${level}`);
+
+        nextSequence();
+        started = true;
+    }
+});
